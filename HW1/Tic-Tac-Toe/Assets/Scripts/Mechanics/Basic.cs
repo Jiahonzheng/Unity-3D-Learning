fd143af58418401d92743743a7c7ac265b186ddd
Jiahonzheng <jiahonzheng@gmail.com>
2019-09-09 00:13:02 +0800

    feat: 完成代码
    完成井字棋代码，实现单人模式（含AI）和双人模式。

---
+++ HW1/Tic-Tac-Toe/Assets/Scripts/Mechanics/Basic.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using UnityEngine;
+
+namespace TicTacToe.Mechanics
+{
+    public enum Player { Unfinished, Tie, First, Second };
+
+    public class Basic
+    {
+        // When it is true, it means the game is still continuing.
+        protected bool playing = true;
+        // When it is true, it means Player1 go first.
+        protected bool turn = true;
+        // It stores the players' move.∏
+        protected readonly Player[,] history = new Player[3, 3];
+
+        public bool GetPlaying()
+        {
+            return playing;
+        }
+
+        public bool GetTurn()
+        {
+            return turn;
+        }
+
+        public Player GetHistory(int i, int j)
+        {
+            return history[i, j];
+        }
+
+        public void SetHistory(int i, int j)
+        {
+            history[i, j] = turn ? Player.First : Player.Second;
+            turn = !turn;
+        }
+
+        public void Reset()
+        {
+            playing = true;
+            turn = true;
+            // Reset the history array.
+            Array.Clear(history, 0, 3 * 3);
+        }
+
+        public Player CheckWin()
+        {
+            // Row Check
+            for (int i = 0; i < 3; ++i)
+            {
+                if (history[i, 0] != Player.Unfinished &&
+                    history[i, 0] == history[i, 1] &&
+                    history[i, 1] == history[i, 2])
+                {
+                    playing = false;
+                    return history[i, 0];
+                }
+            }
+            // Column Check
+            for (int j = 0; j < 3; ++j)
+            {
+                if (history[0, j] != Player.Unfinished &&
+                    history[0, j] == history[1, j] &&
+                    history[1, j] == history[2, j])
+                {
+                    playing = false;
+                    return history[0, j];
+                }
+            }
+            // Cross-Line Check
+            if (history[1, 1] != Player.Unfinished)
+            {
+                if (history[1, 1] == history[0, 0] && history[1, 1] == history[2, 2] ||
+                    history[1, 1] == history[0, 2] && history[1, 1] == history[2, 0])
+                {
+                    playing = false;
+                    return history[1, 1];
+                }
+            }
+            // Check if it is a tie.
+            var numOfUnfinished = 0;
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    if (history[i, j] == Player.Unfinished)
+                    {
+                        numOfUnfinished++;
+                    }
+                }
+            }
+            if (numOfUnfinished == 0)
+            {
+                playing = false;
+                return Player.Tie;
+            }
+            else
+            {
+                return Player.Unfinished;
+            }
+
+        }
+
+        public virtual void AIMove() { }
+    }
+}
---
+++ HW1/Tic-Tac-Toe/Assets/Scripts/UI/GameController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TicTacToe.UI
+{
+    public abstract class GameController : MonoBehaviour
+    {
+        // It controls the logic of the game.
+        protected Mechanics.Basic mechanics;
+
+        // OnGUI is called when rendering and handling GUI events.
+        void OnGUI()
+        {
+            float width = Screen.width * 0.5f - 150;
+            float height = Screen.height * 0.5f - 150;
+            // Width and Height of the "Back to Home" and "Reset" buttons.
+            float buttonWidth = 150;
+            float buttonHeight = 100;
+            // Render "Back to Home" button.
+            float buttonBaseX = width - buttonWidth / 2;
+            float buttonBaseY = height + buttonHeight * 2;
+            if (GUI.Button(new Rect(buttonBaseX - 100, buttonBaseY - 200, buttonWidth, buttonHeight), "Back to Home"))
+            {
+                OnPressBackButton();
+            }
+            // Render "Reset" button.
+            if (GUI.Button(new Rect(buttonBaseX - 100, buttonBaseY, buttonWidth, buttonHeight), "Reset"))
+            {
+                OnPressResetButton();
+            }
+            // Check if there is a winner.
+            var winner = mechanics.CheckWin();
+            if (winner != Mechanics.Player.Unfinished)
+            {
+                string msg = winner == Mechanics.Player.First ? "Player1(X) Wins!" : winner == Mechanics.Player.Second ? "Player2(O) Wins!" : "Tie!";
+                GUIStyle messageStyle = new GUIStyle
+                {
+                    fontSize = 25,
+                    fontStyle = FontStyle.Bold,
+                };
+                messageStyle.normal.textColor = Color.red;
+                // Show the results.
+                GUI.Label(new Rect(width + 50, height - 75, 100, 100), msg, messageStyle);
+                GUI.enabled = false;
+            }
+
+            // Render the map.
+            int bHeight = 100;
+            int bWidth = 100;
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    var player = mechanics.GetHistory(i, j);
+                    string token;
+                    if (player == Mechanics.Player.First)
+                    {
+                        token = "X";
+                    }
+                    else if (player == Mechanics.Player.Second)
+                    {
+                        token = "O";
+                    }
+                    else
+                    {
+                        token = "";
+                    }
+                    // Render the current brick.
+                    var isPressed = GUI.Button(new Rect(width + i * bWidth, height + j * bHeight, bWidth, bHeight), token);
+                    // Prevent multiple clicks.
+                    if (player == Mechanics.Player.Unfinished)
+                    {
+                        AfterRenderButton(i, j, player, isPressed);
+                    }
+                }
+            }
+
+            GUI.enabled = true;
+        }
+
+        public abstract void AfterRenderButton(int i, int j, Mechanics.Player player, bool isPressed);
+
+        // It is called when "Back to Home" button is pressed.
+        void OnPressBackButton()
+        {
+            SceneManager.LoadScene("Home");
+        }
+
+        // It is called when "Reset" button is pressed.
+        void OnPressResetButton()
+        {
+            mechanics.Reset();
+            GUI.enabled = true;
+        }
+    }
+}
