13abf9074d939f1dec10790b4a826b9628da95a9
Jiahonzheng <jiahonzheng@gmail.com>
2019-09-17 11:14:31 +0800

    feat: 完成模拟太阳系代码
    完成模拟太阳系代码。
    添加 README 。

---
+++ HW2/Solar-System/Assets/Scripts/Revolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolution : MonoBehaviour
+{
+    public Transform center;
+    private float speed;
+    private float x;
+    private float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(9, 12);
+        x = Random.Range(-50, 50);
+        y = Random.Range(-50, 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var axis = new Vector3(0, x, y);
+        transform.RotateAround(center.position, axis, speed * Time.deltaTime);
+    }
+}
