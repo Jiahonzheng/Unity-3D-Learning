88184ee775d56ba7e2b6a0427073198e2fdddb45
Jiahonzheng <jiahonzheng@gmail.com>
2019-10-26 15:15:26 +0800

    feat: 添加基础代码
    添加基础代码。

---
+++ HW6/Patrol/Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Patrol
+{
+    public class GameController : MonoBehaviour, ISceneController
+    {
+        void Awake()
+        {
+            Director.GetInstance().OnSceneWake(this);
+        }
+
+        public void LoadResources()
+        {
+            GameObject map = Instantiate(Resources.Load<GameObject>("Prefabs/Map"));
+            GameObject fence = Instantiate(Resources.Load<GameObject>("Prefabs/Fence"));
+            fence.transform.parent = map.transform;
+        }
+    }
+}
