63895edf57ad37cf29a7cffc8fabe35c2c6b3259
Jiahonzheng <jiahonzheng@gmail.com>
2019-10-27 12:25:45 +0800

    feat: 添加基础代码
    添加基础代码。

---
+++ HW6/Patrol/Assets/Scripts/Actions/Action.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Patrol
+{
+    public interface IActionCallback
+    {
+        // 用于通知更高级对象动作已执行完毕。
+        void ActionDone(Action action);
+    }
+
+    public class Action : ScriptableObject
+    {
+        public bool enable = true;
+        // 若为 true ，表示动作已完成。
+        public bool destroy = false;
+        // 表示需要进行运动的游戏对象。
+        public GameObject gameObject { get; set; }
+        public Transform transform { get; set; }
+        // 表示在动作执行完毕后，需要通知的对象。
+        public IActionCallback callback;
+
+        // 在此方法中实现动作的初始化操作。
+        public virtual void Start()
+        {
+            // 提示用户需要实现此方法！
+            throw new System.NotImplementedException();
+        }
+
+        // 在此方法中实现动作逻辑。
+        public virtual void Update()
+        {
+            // 提示用户需要实现此方法！
+            throw new System.NotImplementedException();
+        }
+    }
+}
