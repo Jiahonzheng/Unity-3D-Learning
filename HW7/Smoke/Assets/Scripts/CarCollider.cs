d0ff896513c42a2dc6d14cd75b278f3f7dfa0a65
Jiahonzheng <jiahonzheng@gmail.com>
2019-11-09 15:27:31 +0800

    feat: 完成代码
    完成代码，并添加注释。

---
+++ HW7/Smoke/Assets/Scripts/CarCollider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Smoke
+{
+    public class CarCollider : MonoBehaviour
+    {
+        // 记录车辆损坏情况。
+        private float damage = 0;
+
+        // 返回车辆损坏情况。
+        public float GetDamage()
+        {
+            return damage;
+        }
+
+        // 设置车辆损坏情况。
+        public void SetDamage(float d)
+        {
+            damage = d;
+        }
+
+        // 当车辆与墙体碰撞时，增加其损坏系数。
+        private void OnCollisionEnter(Collision collision)
+        {
+            damage += 4.5f * gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+        }
+    }
+}
