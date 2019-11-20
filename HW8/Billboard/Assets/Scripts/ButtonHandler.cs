cde8071c92fdd51f874541201f1dd8ff3ddbc3f9
Jiahonzheng <jiahonzheng@gmail.com>
2019-11-20 22:56:08 +0800

    feat: 完成代码实现
    完成代码实现。

---
+++ HW8/Billboard/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    // 消息主体。
+    public Text text;
+
+    void Start()
+    {
+        // 绑定响应点击事件。
+        gameObject.GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    // 响应点击事件。
+    void OnClick()
+    {
+        // 当消息主体已被展开。
+        if (text.gameObject.activeSelf)
+        {
+            text.gameObject.SetActive(false);
+        }
+        else // 当消息主体未被展开。
+        {
+            text.gameObject.SetActive(true);
+        }
+    }
+}
