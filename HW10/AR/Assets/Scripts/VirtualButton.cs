87d477a07e4cf8eb3ce0cf66923fb8de487b0e00
Jiahonzheng <jiahonzheng@gmail.com>
2019-12-20 21:54:13 +0800

    feat: 完成基本代码
    完成基本代码。

---
+++ HW10/AR/Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour
+{
+    public VirtualButtonBehaviour[] vbbs;
+    public Animator animator;
+
+    void Start()
+    {
+        vbbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbbs.Length; ++i)
+        {
+            vbbs[i].RegisterOnButtonPressed(OnButtonPressed);
+            vbbs[i].RegisterOnButtonReleased(OnButtonReleased);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        switch (vb.VirtualButtonName)
+        {
+            case "DanceButton":
+                animator.SetBool("isWalking", false);
+                animator.SetBool("isDancing", true);
+                break;
+            case "WalkButton":
+                animator.SetBool("isDancing", false);
+                animator.SetBool("isWalking", true);
+                break;
+            default:
+                animator.SetBool("isWalking", false);
+                animator.SetBool("isDancing", false);
+                break;
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+    }
+}
